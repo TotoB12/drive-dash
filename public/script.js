b89afe246c5a827fd0553237968278200f62391d
TotoB12 <totobeliard@outlook.com>
2024-11-13 09:08:21 -0500

    Update package.json to set main entry point to server.js and enhance Mapbox integration with improved zoom and pitch settings

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
-// Replace 'YOUR_MAPBOX_ACCESS_TOKEN' with your actual Mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1IjoidG90b2IxMjE3IiwiYSI6ImNsbXo4NHdocjA4dnEya215cjY0aWJ1cGkifQ.OMzA6Q8VnHLHZP-P8ACBRw';
 
 const map = new mapboxgl.Map({
-    container: 'map', // Container ID
-    style: 'mapbox://styles/totob1217/cm3f0b1qp000v01rv4evcaegr', // mapbox://styles/mapbox/navigation-day-v1
-    center: [0, 0], // Starting position [longitude, latitude]
-    zoom: 15 // Starting zoom level
+    container: 'map',
+    style: 'mapbox://styles/totob1217/cm3f0b1qp000v01rv4evcaegr',
+    center: [0, 0],
+    zoom: 4,
+    pitch: 60
 });
 
 const geolocate = new mapboxgl.GeolocateControl({
@@ -13,12 +13,19 @@ const geolocate = new mapboxgl.GeolocateControl({
         enableHighAccuracy: true
     },
     trackUserLocation: true,
-    showUserHeading: true
+    showUserHeading: true,
+    fitBoundsOptions: {
+      maxZoom: 17,
+      pitch: 60
+    }
 });
+const nav = new mapboxgl.NavigationControl();
+const fullscreen = new mapboxgl.FullscreenControl();
 
 map.addControl(geolocate);
+map.addControl(nav);
+map.addControl(fullscreen);
 
-// Center the map on the user's location as soon as it's available
 map.on('load', () => {
     geolocate.trigger();
 });
